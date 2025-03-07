08433352f062ab5dd808997fbb723c732fd03872
topliceanuvlad19 <topliceanuvlad19@gmail.com>
2025-03-07 09:56:45 +0200

    modify the sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Trap SIGTERM and forward it to the Gunicorn process
-trap 'echo "SIGTERM received, shutting down gunicorn..."; kill -TERM $PID' SIGTERM
+# Trap TERM (instead of SIGTERM) and forward it to Gunicorn
+trap 'echo "TERM received, shutting down gunicorn..."; kill -TERM $PID' TERM
 
 # Start Gunicorn in the background and capture its PID
 gunicorn --bind 0.0.0.0:8080 calculator:app &
